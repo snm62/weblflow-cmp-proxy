94e26f0895ef0fd672760b58033e2c6734935b77
snm62 <sandra@seattlenewmedia.com>
2025-02-11 23:03:59 +0530

    Update cmp-loader.js

--- cmp-loader.js
+++ cmp-loader.js
@@ -2,7 +2,10 @@
   try {
     const response = await fetch("https://manual-worker.web-8fb.workers.dev/cmp-script.js");
     const scriptText = await response.text();
-    eval(scriptText);  // Execute the script dynamically
+    
+    const script = document.createElement("script");
+    script.textContent = scriptText;
+    document.head.appendChild(script);
   } catch (error) {
     console.error("Failed to load CMP script", error);
   }
